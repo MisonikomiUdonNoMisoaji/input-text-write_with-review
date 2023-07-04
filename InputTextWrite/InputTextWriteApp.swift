a5d5032b1536e8230d69efa1eb6643e93af7d817
riataso <rarararai0070@gmail.com>
2023-07-04 22:59:58 +0900

    Initial Commit

---
+++ InputTextWrite/InputTextWriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  InputTextWriteApp.swift
+//  InputTextWrite
+//
+//  Created by 佐藤来 on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct InputTextWriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
