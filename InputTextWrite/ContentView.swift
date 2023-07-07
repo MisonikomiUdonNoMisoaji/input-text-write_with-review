7c09a01f801743c4c05a149d01637c854a2aaed9
riataso <rarararai0070@gmail.com>
2023-07-07 13:37:59 +0900

    課題を反映

--- InputTextWrite/ContentView.swift
+++ InputTextWrite/ContentView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        @State var text: String = ""
+    @State var judgement: Bool = true
+    @State var buttonlabel: String = "編集"
 
+    var body: some View {
+            TextEditor(text: $text)
+                .frame(width: 300, height: 200)     // フレームサイズ指定
+                .border(Color.gray, width: 1)
+                .disabled(judgement)// フレーム外枠の色と太さ指定
+        Button {
+            if (judgement == true) {
+                judgement = false
+                buttonlabel = "編集完了"
+            }
+            else {
+                judgement = true
+                buttonlabel = "編集"
+            }
+            
+        } label: {
+            Text(buttonlabel)
+        }
+
+        }
+    
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
